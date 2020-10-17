498a2890aed39b54414f79f72a4237d1b0541a07
costarzhang <costarzhang@foxmail.com>
2020-10-17 22:53:47 +0800

    push

--- zhang/graph/BST.cpp
+++ zhang/graph/BST.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 
 // 二叉排序树
+/*
+一颗二叉排序树：
+    若左子树非空，则左子树上所有结点的关键字值小于根结点的关键字值
+    若右子树非空，则右子树上所有结点的关键字值大于根结点的关键字值
+    左右子树本身右是一颗二叉排序树
+*/
 typedef struct BTNode {
     int key;
     struct BTNode *left;
@@ -48,9 +54,9 @@ int BSTinsert (BTNode *&bt, int key) {
         bt->right = bt->left = nullptr;
         return 1;
     } else {
-        if(key == bt->key) {
+        if(key == bt->key) { // 二叉排序树中各结点关键字值不相等
             return 0;
-        } else if (key < bt->key) {
+        } else if (key < bt->key) { // 插入结点的关键字值小于根节点值，则向根的左子树中插入
             return BSTinsert(bt->left, key);
         } else {
             return BSTinsert(bt->right, key);
@@ -59,34 +65,57 @@ int BSTinsert (BTNode *&bt, int key) {
 }
 
 // 删除结点
+/*
+           53                                           53
+        /     \      删除45（仅有一棵子树）           /    \ 
+      17       78    ------------------------->     17      78
+    /   \     /  \                                /   \    /  \
+   9     45  65   87                             9    23  65   87
+        /
+       23
+
+             53                                                  53                                             53
+           /    \                                              /    \                                         /    \
+         17      78       删除78（有两棵子树）                17     81        从树中删除81                  17     81       
+       /   \    /   \   ---------------------->             /   \   /   \    --------------------->        /   \   /   \
+      9    45  65    94   以81替代78                       9    45 65    94                               9    45 65    94
+          /         /                                          /        /                                     /        /
+         23        81                                         23       81                                    23      88
+                     \                                                   \
+                      88                                                  88  
+1.叶子结点直接删除
+2.被删除结点仅有一颗子树，则以子树替代被删除结点
+3.若被删除结点有两颗子树，则以该节点的直接后继（或者直接前驱）替代该结点
+然后从树中删除这个直接后继（或者直接前驱），转换为1或者2的情况。这里的直接后继是指中序遍历序列的直接后继
+*/
 void BSTdelete(BTNode *&bt, int key) {
     BTNode *q, *s;
     if (bt == nullptr) {
         return;
     }else if (bt->key == key) {
-        if (!bt->right && !bt->left) {
+        if (!bt->right && !bt->left) { // 找到结点直接删除
             q = bt;
             bt = nullptr;
             free(q);
         } else if (!bt->right) {  // 待删除结点仅有左子树
             q = bt;
-            bt = bt->left;
+            bt = bt->left; // 以其左子树代替
             free(q);
         }else if (!bt->left) { // 待删除结点仅有右子树
             q = bt;
-            bt = bt->right;
+            bt = bt->right; // 以其右子树代替
             free(q);
         } else { // 待删除结点的左右子树均不为空
             q = bt;
             s = bt->left;
-            while (s->right != nullptr) {
+            while (s->right != nullptr) { // 寻找结点前驱，q指向bt的直接前驱的父节点，bt的直接前驱有两种可能：1.bt的左孩子2.bt的左孩子的右孩子或者左孩子的右孩子的右孩子...
                 q = s;
-                s = s->left;
+                s = s->right;
             }
-            bt->key = s->key;
-            if (q != bt) {
+            bt->key = s->key; // 以前驱替代
+            if (q != bt) {//前驱是第二种情况
                 q->right = s->left;
-            } else {
+            } else {//前驱是第一种情况
                 q->left = s->left;
             }
         }
@@ -114,14 +143,13 @@ void BSTsort(BTNode *bt) {
     }
 }
 int main() {
-    int key[7] = {5, 4, 8, 3, 7, 9, 10};
+    int key[10] = {53, 17, 78, 9, 45, 46, 65, 60, 66, 94};
     BTNode *bt = nullptr;
-    createBST(bt, key, 7);
+    createBST(bt, key, 10);
     BFT(bt);
-    BSTdelete(bt, 5);
+    BSTdelete(bt, 78);
     cout << endl;
     BFT(bt);
     cout << endl;
-    BSTsort(bt);
     return 0;
 }
---
+++ zhang/tree/sets.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// 并查集
+
+int sets[10];
+
+void init(int s[]) {
+    for (int i = 0; i < s.length(); i++)
+        s[i] = -1; // 每个结点各自组成一个集合
+}
+
+void find(int s[], int x) {
+    while (s[x] >= 0) {
+        x = s[x];
+    }
+    return x;
+}
+
+void unin(int s[], int root1, int root2) {
+    s[rooot2] = root1;// 将root2作为root1的一个孩子
+}
