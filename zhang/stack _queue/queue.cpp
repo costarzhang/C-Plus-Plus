d7ce822c6d250e82a977ffda857b6ffe9b35a2b4
costarzhang <costarzhang@foxmail.com>
2020-11-09 22:48:12 +0800

    push

--- zhang/stack _queue/queue.cpp
+++ zhang/stack _queue/queue.cpp
@@ -1,51 +1,79 @@
 #include <iostream>
 using namespace std;
-#define maxsize 20
+#define maxsize 4
 
-struct queue {
+// 队列
+struct queue
+{
     int *qu;
     int front, rear;
 };
 
-void initQueue(queue &queue) {
+void initQueue(queue &queue)
+{
     queue.front = queue.rear = 0; // 队列空
     queue.qu = (int *)malloc(sizeof(int) * maxsize);
 }
 
-int isEmpty(queue &queue) {
-    if (queue.front == queue.rear) {
+int isEmpty(queue &queue)
+{
+    if (queue.front == queue.rear)
+    {
         return 1;
-    } else {
+    }
+    else
+    {
         return 0;
     }
 }
 
-int enQueue (queue &queue, int x) {
-    if ((queue.rear+1) % maxsize == queue.front) {
+int isFull(queue queue)
+{
+    if ((queue.rear + 1) % maxsize == queue.front)
+    {
+        return 1;
+    }
+    else
+    {
         return 0;
     }
-    queue.rear = (queue.rear+1) % maxsize;
+}
+int enQueue(queue &queue, int x)
+{
+    if ((queue.rear + 1) % maxsize == queue.front)
+    {
+        return 0;
+    }
+    queue.rear = (queue.rear + 1) % maxsize;
     queue.qu[queue.rear] = x;
     return 1;
 }
 
-int deQueue(queue &queue, int &x) {
-    if (queue.front == queue.rear) {
+int deQueue(queue &queue, int &x)
+{
+    if (queue.front == queue.rear)
+    {
         return 0;
     }
-    queue.front = (queue.front+1) % maxsize;
+    queue.front = (queue.front + 1) % maxsize;
     x = queue.qu[queue.front];
     return 1;
 }
 
-int main() {
+int main()
+{
     queue q;
     initQueue(q);
     int x;
-    enQueue(q, 5);
-    enQueue(q, 6);
-    deQueue(q, x);
-    cout << x << endl;
-    deQueue(q, x);
-    cout << x << endl;
+    for (int i = 0; i < 4; i++)
+    {
+        if (enQueue(q, i))
+        {
+            cout << "success!" << endl;
+        }
+        else
+        {
+            cout << "fail!" << endl;
+        }
+    }
 }
--- zhang/string/match.cpp
+++ zhang/string/match.cpp
@@ -2,37 +2,49 @@
 using namespace std;
 
 // 简单模式匹配
-int index(string text, string pattern) {
-    int iText = 0; // 文本字符指针
+int index(string text, string pattern)
+{
+    int iText = 0;    // 文本字符指针
     int iPattern = 0; //模式串字符指针
-    int k = iText; // 记录每趟开始匹配时文本指针的位置
+    int k = iText;    // 记录每趟开始匹配时文本指针的位置
 
-    while (iText <= text.length() && iPattern <= pattern.length()) {
-        for (int i = k; i < text.length(); i++) {
+    while (iText <= text.length() && iPattern <= pattern.length())
+    {
+        for (int i = k; i < text.length(); i++)
+        {
             cout << text[i];
         }
         cout << endl;
-        for (int j = 0; j < pattern.length(); j++) {
+        for (int j = 0; j < pattern.length(); j++)
+        {
             cout << pattern[j];
         }
         cout << endl;
-        if (text[iText] == pattern[iPattern]) { // 字符匹配，指针后移
+        // 匹配算法核心代码
+        if (text[iText] == pattern[iPattern])
+        { // 字符匹配，指针后移
             ++iText;
             ++iPattern;
-        } else { // 字符不匹配，文本指针后移一位
-                iText = ++k;
-                iPattern = 0; // 模式串指针回到模式串初始位置
+        }
+        else
+        { // 字符不匹配，文本指针后移一位
+            iText = ++k;
+            iPattern = 0; // 模式串指针回到模式串初始位置
         }
     }
-    if (iPattern > pattern.length() - 1) {
+
+    if (iPattern > pattern.length() - 1) //模式指针到达模式串尾，匹配结束
+    {
         return k;
-    } else {
+    }
+    else
+    {
         return pattern.length();
     }
 }
 
-
-int main() {
+int main()
+{
     string text = "ABABCABCACBAB";
     string pattern = "ABCAC";
     cout << index(text, pattern);
