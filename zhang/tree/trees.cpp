a8a315fedbcdac4a59cc4d07aa1da6889b4b4f47
costarzhang <costarzhang@foxmail.com>
2020-12-01 16:55:33 +0800

    push

---
+++ zhang/tree/trees.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+// 树的孩子兄弟表示法
+typedef struct TNode
+{
+    char data;
+    struct TNode *firstchild, *nextsibling;
+} * Tree;
+
+// 树的创建
+void create(Tree &tree, bool iffirstchild, bool ifnextsibling, TNode *pre)
+{
+    char data;
+    if (iffirstchild)
+    {
+        cout << "输入" << pre->data << "的第一个孩子结点：";
+        cin >> data;
+    }
+    else if (ifnextsibling)
+    {
+        cout << "输入" << pre->data << "的右兄弟结点：";
+        cin >> data;
+    }
+    else
+    {
+        cout << "输入根结点：";
+        cin >> data;
+    }
+
+    tree = (TNode *)malloc(sizeof(TNode));
+    tree->data = data;
+    tree->firstchild = nullptr;
+    tree->nextsibling = nullptr;
+
+    bool child, sibling;
+    cout << tree->data << "是否有孩子和右兄弟结点：";
+    cin >> child >> sibling;
+    if (child)
+    {
+        create(tree->firstchild, child, false, tree);
+    }
+    if (sibling)
+    {
+        create(tree->nextsibling, false, sibling, tree);
+    }
+}
+
+void pre(Tree tree)
+{
+    if (tree != nullptr)
+    {
+        cout << tree->data << "  ";
+        pre(tree->firstchild);
+        pre(tree->nextsibling);
+    }
+}
+int main()
+{
+    Tree tree = nullptr;
+    create(tree, false, false, tree);
+    pre(tree);
+}
